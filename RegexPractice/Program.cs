82390c21c9d1b5d6bfabf6f36b7492ba3216b60a
chaitanya4294 <chaitanya.4294@gmail.com>
2021-10-09 08:04:37 +0530

    [Chaitanya] Add : Added first name validation method

--- RegexPractice/Program.cs
+++ RegexPractice/Program.cs
@@ -6,7 +6,25 @@ namespace RegexPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            bool isRun = true;
+            while (isRun)
+            {
+                UserRegistration user = new UserRegistration();
+                Console.WriteLine("Select Program Option: (1. FirstName Validation)");
+                int programOption = Convert.ToInt32(Console.ReadLine());
+                switch (programOption)
+                {
+                    case 1:
+                        Console.WriteLine("Enter your first name:");
+                        string firstName = Console.ReadLine();
+                        user.FirstNameValidation(firstName);
+                        break;
+                    default:
+                        isRun = !isRun;
+                        break;
+                }
+            }
         }
     }
 }
---
+++ RegexPractice/UserRegistration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexPractice
+{
+    class UserRegistration
+    {
+        const string NAME_FORMAT = "^[A-Z]{1}[a-z]{3,}$";
+        public void FirstNameValidation(string firstName)
+        {
+            if (Regex.IsMatch(firstName, NAME_FORMAT))
+            {
+                Console.WriteLine("Entered firstname is as per requirements");
+                return;
+            }
+            Console.WriteLine("Entered firstname is not as per requirements");
+        }
+    }
+}
