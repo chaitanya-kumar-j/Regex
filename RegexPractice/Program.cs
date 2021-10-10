90d3733a9c64d65d40fbd2cd94934ea6f63fc620
chaitanya4294 <chaitanya.4294@gmail.com>
2021-10-10 18:39:45 +0530

    [Chaitanya] Add : Added email validation method

--- RegexPractice/Program.cs
+++ RegexPractice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace RegexPractice
 {
@@ -10,6 +11,7 @@ namespace RegexPractice
             bool isRun = true;
             while (isRun)
             {
+                string emailsFilePath = @"G:\BridgeLabz\Regex\RegexPractice\ValidEmailsList.txt";
                 UserRegistration user = new UserRegistration();
                 Console.WriteLine("Select Program Option: (1. First name Validation, 2. Last name Validation)");
                 int programOption = Convert.ToInt32(Console.ReadLine());
@@ -24,6 +26,29 @@ namespace RegexPractice
                         Console.WriteLine("Enter your last name:");
                         string lastName = Console.ReadLine();
                         user.LastNameValidation(lastName);
+                        break;
+                    case 3:
+                        string[] validEmailList = File.ReadAllText(emailsFilePath).Split("\n")[0].Split(",");
+                        string[] inValidEmailList = File.ReadAllText(emailsFilePath).Split("\n")[1].Split(",");
+                        string[] bothValidAndInValidEmailList = File.ReadAllText(emailsFilePath).Split("\n")[2].Split(",");
+                        Console.WriteLine("Validation of validEmailList: ");
+                        foreach (string email in validEmailList)
+                        {
+                            Console.Write($"\n{email} - ");
+                            user.EmailValidation(email);
+                        }
+                        Console.WriteLine("\nValidation of inValidEmailList: ");
+                        foreach (string email in inValidEmailList)
+                        {
+                            Console.Write($"\n{email} - ");
+                            user.EmailValidation(email);
+                        }
+                        Console.WriteLine("\nValidation of bothValidAndInValidEmailList: ");
+                        foreach (string email in bothValidAndInValidEmailList)
+                        {
+                            Console.Write($"\n{email} - ");
+                            user.EmailValidation(email);
+                        }
                         break;
                     default:
                         isRun = !isRun;
--- RegexPractice/UserRegistration.cs
+++ RegexPractice/UserRegistration.cs
@@ -8,6 +8,7 @@ namespace RegexPractice
     class UserRegistration
     {
         const string NAME_FORMAT = "^[A-Z]{1}[a-z]{2,}$";
+        const string EMAIL_ID_FORMAT = "^[A-Z0-9a-z]{1,}([.#$^_-][A-Za-z0-9]+)?[@][A-Za-z]{2,}[.][A-Za-z]{2,3}([.][a-zA-Z]{2})?$";
         public void FirstNameValidation(string firstName)
         {
             if (Regex.IsMatch(firstName, NAME_FORMAT))
@@ -25,6 +26,15 @@ namespace RegexPractice
                 return;
             }
             Console.WriteLine("Entered lastname is not as per requirements");
+        }
+        public void EmailValidation(string emailId)
+        {
+            if (Regex.IsMatch(emailId, EMAIL_ID_FORMAT))
+            {
+                Console.WriteLine("Entered email is as per requirements");
+                return;
+            }
+            Console.WriteLine("Entered email is not as per requirements");
         }
     }
 }
