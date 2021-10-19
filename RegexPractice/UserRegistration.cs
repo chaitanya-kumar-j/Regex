8253f8bf450de9406284d62c0d5374867c372cc7
chaitanya4294 <chaitanya.4294@gmail.com>
2021-10-19 15:23:02 +0530

    [Chaitanya] Add : Added Nunit test cases for all fields of user registration

--- RegexPractice/UserRegistration.cs
+++ RegexPractice/UserRegistration.cs
@@ -5,57 +5,62 @@ using System.Text.RegularExpressions;
 
 namespace RegexPractice
 {
-    class UserRegistration
+    public class UserRegistration
     {
         const string NAME_FORMAT = "^[A-Z]{1}[a-z]{2,}$";
         const string EMAIL_ID_FORMAT = "^[A-Z0-9a-z]{1,}([.#$^_-][A-Za-z0-9]+)?[@][A-Za-z]{2,}[.][A-Za-z]{2,3}([.][a-zA-Z]{2,3})?$";
         const string MOBILE_NUMBER_FORMAT = "^[0-9]{2}[ ][6-9][0-9]{9}$";
         const string PASSWORD_FORMAT = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9]){1}.{8,}$";
 
-        public void FirstNameValidation(string firstName)
+        public bool FirstNameValidation(string firstName)
         {
             if (Regex.IsMatch(firstName, NAME_FORMAT))
             {
                 Console.WriteLine("Entered firstname is as per requirements");
-                return;
+                return true;
             }
             Console.WriteLine("Entered firstname is not as per requirements");
+            return false;
         }
-        public void LastNameValidation(string lastName)
+        public bool LastNameValidation(string lastName)
         {
             if (Regex.IsMatch(lastName, NAME_FORMAT))
             {
                 Console.WriteLine("Entered lastname is as per requirements");
-                return;
+                return true;
             }
             Console.WriteLine("Entered lastname is not as per requirements");
+            return false;
         }
-        public void EmailValidation(string emailId)
+        public bool EmailValidation(string emailId)
         {
             if (Regex.IsMatch(emailId, EMAIL_ID_FORMAT))
             {
                 Console.WriteLine("Entered email is as per requirements");
-                return;
+                return true;
             }
             Console.WriteLine("Entered email is not as per requirements");
+            return false;
         }
-        public void MobileNumberValidation(string mobileNumber)
+        public bool MobileNumberValidation(string mobileNumber)
         {
             if (Regex.IsMatch(mobileNumber, MOBILE_NUMBER_FORMAT))
             {
                 Console.WriteLine("Entered Mobile number is as per requirements");
-                return;
+                return true;
             }
             Console.WriteLine("Entered Mobile number is not as per requirements");
+            return false;
         }
-        public void PasswordValidation(string password)
+        public bool PasswordValidation(string password)
         {
             if (Regex.IsMatch(password, PASSWORD_FORMAT))
             {
                 Console.WriteLine("Entered Password is as per requirements");
-                return;
+                return true;
             }
             Console.WriteLine("Entered Password is not as per requirements");
+            return false;
         }
     }
 }
---
+++ UserRegistrationTesting/ValidationTestCases.cs
@@ -0,0 +1,84 @@
+using NUnit.Framework;
+using RegexPractice;
+
+namespace UserRegistrationTesting
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GivenFirstNameInCorrectFormat_WhenFirstNameValidation_ThenShouldReturnTrue()
+        {
+            string testFirstName = "Chaitanya";
+            bool result = new UserRegistration().FirstNameValidation(testFirstName);
+            Assert.AreEqual(true, result);
+        }
+        [Test]
+        public void GivenFirstNameInWrongFormat_WhenFirstNameValidation_ThenShouldReturnFalse()
+        {
+            string testFirstName = "Ch";
+            bool result = new UserRegistration().FirstNameValidation(testFirstName);
+            Assert.AreEqual(false, result);
+        }
+        [Test]
+        public void GivenLastNameInCorrectFormat_WhenLastNameValidation_ThenShouldReturnTrue()
+        {
+            string testLastName = "Kumar";
+            bool result = new UserRegistration().LastNameValidation(testLastName);
+            Assert.AreEqual(true, result);
+        }
+        [Test]
+        public void GivenLastNameInWrongFormat_WhenLastNameValidation_ThenShouldReturnFalse()
+        {
+            string testLastName = "kumar";
+            bool result = new UserRegistration().LastNameValidation(testLastName);
+            Assert.AreEqual(false, result);
+        }
+        [Test]
+        public void GivenEmailIdInCorrectFormat_WhenEmailValidation_ThenShouldReturnTrue()
+        {
+            string testEmail = "Chaitanya.Kumar@gmail.com";
+            bool result = new UserRegistration().EmailValidation(testEmail);
+            Assert.AreEqual(true, result);
+        }
+        [Test]
+        public void GivenEmailIdInWrongFormat_WhenEmailValidation_ThenShouldReturnFalse()
+        {
+            string testEmail = "kumar@gmail.@.com";
+            bool result = new UserRegistration().EmailValidation(testEmail);
+            Assert.AreEqual(false, result);
+        }
+        [Test]
+        public void GivenMobileNumberInCorrectFormat_WhenMobileNumberValidation_ThenShouldReturnTrue()
+        {
+            string testMobileNumber = "91 8234567890";
+            bool result = new UserRegistration().MobileNumberValidation(testMobileNumber);
+            Assert.AreEqual(true, result);
+        }
+        [Test]
+        public void GivenMobileNumberInWrongFormat_WhenMobileNumberValidation_ThenShouldReturnFalse()
+        {
+            string testMobileNumber = "91 1234567890";
+            bool result = new UserRegistration().MobileNumberValidation(testMobileNumber);
+            Assert.AreEqual(false, result);
+        }
+        [Test]
+        public void GivenPasswordInCorrectFormat_WhenPasswordValidation_ThenShouldReturnTrue()
+        {
+            string testPassword = "Abcd_123";
+            bool result = new UserRegistration().PasswordValidation(testPassword);
+            Assert.AreEqual(true, result);
+        }
+        [Test]
+        public void GivenPasswordInWrongFormat_WhenPasswordValidation_ThenShouldReturnFalse()
+        {
+            string testPassword = "Abcd_abcd";
+            bool result = new UserRegistration().PasswordValidation(testPassword);
+            Assert.AreEqual(false, result);
+        }
+    }
+}
